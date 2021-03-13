b907c38ece4665146079bd11e451b2dee53e25a0
nicasalazar <60679947+nicasalazar@users.noreply.github.com>
2021-03-12 21:59:33 -0800

    yes

---
+++ JS/app.js
@@ -0,0 +1,46 @@
+console.log('Week 9 Lab');
+
+function firstTest() {
+        let num1 = 10;
+        let num2 = 20;
+        let total = num1 + num2;
+        alert('Total is : ' + total);
+      }
+
+      function secondTest() {
+        let num1 = 10;
+        let num2 = 20;
+        let total = num1 - num2;
+        alert('Total is : ' + total);
+      }
+
+    function thirdTest() {
+    
+      let iceCream = 'chocolate';
+      if (iceCream === 'chocolate') {
+        alert('Yay, I love chocolate ice cream!')
+    }
+}
+ function ifTest(age) {
+            age = Number(age);
+            if (age == age) {
+                print = "They match!";
+            } else {
+                if (age != age) {
+                    print = "no luck";
+                }
+            }
+        }
+    function forTest() {
+        for (let i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+              console.log('The current index/num is EVEN : ' + i);
+            } 
+          }
+    }
+    firstTest();
+    secondTest();
+    thirdTest();
+    ifTest();
+    forTest();
+
